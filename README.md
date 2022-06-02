0cd790657909f15ea0142b42c4c5223755a9f0ee
Rodrigo <rodrigorael53@gmail.com>
2022-06-02 00:45:10 -0300

    taking a test to select the premium and enter the email in the input

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Alguns posts atrás eu falei sobre testes E2E e sobre mais 2 tipos de testes, ma
 Optei por utilizar o expo no bare workflow porque vou realizar testes E2E e vai ser necessário fazer algumas configurações nativas para cada ambiente.
 Utilizando a biblioteca Detox, que é uma biblioteca focada em testes E2E, um fato interessante é que ela utiliza o jest que é outra biblioteca para realizar testes.
 [Veja mais dessa biblioteca aqui!](https://github.com/wix/Detox)
+
+Uma das coisas mais delicadas nos testes E2E que testam simulando como se fosse o usuário interagindo com a aplicação, sempre foi que algumas ações são assíncronas, é muito difícil ajustar o timing do teste automatizado aguardar um click do usuário, simular o click do usuário e esperar esse click acontecer. E o detox é muito bom nesse timing.
--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,23 +1,41 @@
-describe('Example', () => {
+describe('My first suite tests E2E', () => {
   beforeAll(async () => {
+    // abrindo o app
     await device.launchApp();
   });
 
+  // antes de cada teste
   beforeEach(async () => {
+    // fazendo um reload no app
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should have info about cancel subscription', async () => {
+    const info = await element(by.text('Cancel anytime. Ofter terms apply.'));
+    await expect(info).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
+  it('should subscribe to the premium plan', async () => {
+    // selecionando um elemento pelo id e dando um click(tap())
+    await element(by.id('option-premium')).tap();
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    // selecionando o elemento input e clicando nele, porque o usuário não pode
+    // digitar nele se ele não estiver em foco
+    await element(by.id('input-email')).tap();
+    // o input já está em foco e agora eu coloco o que o teste tem que digitar
+    // dentro do input
+    await element(by.id('input-email')).typeText('rodrigo@email.com');
+
+    // selecionando o elemento TouchableWithoutFeedback(keyboard) e dando um
+    // click nele, para ele fechar o teclado
+    await element(by.id('keyboard')).tap();
+    // selecionando o elemento button e dando um click nele
+    await element(by.id('button-subscribe')).tap();
+
+    // delay
+    await new Promise((r) => setTimeout(r, 5000)); // 5000 => 5ms
+
+    // estou esperando que o elemento que selecionei seja visível na tela
+    await expect(element(by.id('confirmation-message'))).toBeVisible();
   });
 });
